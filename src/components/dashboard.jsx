6860976bd5f734e8e03506c6e9f4032b4f8ed785
Shuvam Dutta <shuvamn.dutta@codeclouds.co.in>
2023-09-14 13:19:31 +0530

    api intigration

--- src/components/dashboard.jsx
+++ src/components/dashboard.jsx
@@ -1,47 +1,81 @@
-import { useEffect, useState } from 'react';
+import { useEffect, useRef, useState } from 'react';
 import { useFormik } from 'formik'
+import axios from 'axios';
 import { processEmailSchema } from '../schemas';
-
+import './dashboard.css'
 const initialValues = {
-    originating_crm: '',
-    new_crm: '',
-    new_campaign: '',
-    new_product: '',
-    max_per_day: '',
-    start_hours: '',
-    start_minutes: '',
-    end_hours: '',
-    end_minutes: '',
-    matched: '',
-    csv: '',
-    emails_list: '',
+    pluginName: '',
+    campaignId: '',
+    affId: '',
+    product1_id: '',
+    product1_qty: '',
+    salesUrl: '',
+    maxPerDay: '',
+    startHour: '',
+    startMins: '',
+    endHour: '',
+    endMins: '',
+    // matched: '',
+    csvFile: '',
+    // emails_list: '',
 }
 
 const Dashboard = () => {
 
-    const [showInput, setInput] = useState(false);
+    // const [showInput, setInput] = useState(false);
+    const [successMessage, setSuccessMessage] = useState();
+    const csvRef = useRef();
 
-    const { values, errors, touched, handleChange, handleBlur, handleSubmit } = useFormik({
+    const { values, errors, touched, handleChange, handleBlur, handleSubmit, setFieldValue } = useFormik({
         initialValues: initialValues,
         validationSchema: processEmailSchema,
-        onSubmit: (values, action) => {
+        onSubmit: async (values, action) => {
+            console.log(values.csvFile);
             console.log(values);
-            action.resetForm();
+            const formData = new FormData();
+            formData.append('csvFile', values.csvFile);
+            formData.append('pluginName', values.pluginName);
+            formData.append('campaignId', values.campaignId);
+            formData.append('affId', values.affId);
+            formData.append('product1_id', values.product1_id);
+            formData.append('product1_qty', values.product1_qty);
+            formData.append('salesUrl', values.salesUrl);
+            formData.append('maxPerDay', values.maxPerDay);
+            formData.append('startHour', values.startHour);
+            formData.append('startMins', values.startMins);
+            formData.append('endHour', values.endHour);
+            formData.append('endMins', values.endMins);
+            console.log('formData == ', formData);
+            try {
+                const resp = await axios.post(`${import.meta.env.VITE_API}/addcustomer`, formData);
+                console.log('Response == ', resp.data);
+                if (resp.data) {
+                    setSuccessMessage(resp.data.message)
+                    action.resetForm();
+                    csvRef.current.value = null;
+                }
+            } catch (error) {
+                console.log('error == ', error);
+                if (error) {
+                    setSuccessMessage('Something went wrong!')
+                }
+            }
         }
     });
 
-    const selectChangeHandler = (e) => {
-        if (e.target.value) {
-            if (e.target.value == 'email') {
-                setInput('email')
-            }
-            if (e.target.value == 'csv') {
-                setInput('csv')
-            }
-        } else {
-            setInput(false)
-        }
-    }
+    // const selectChangeHandler = (e) => {
+    //     console.log('Change');
+    //     if (e.target.value) {
+    //         if (e.target.value == 'email') {
+    //             setInput('email')
+    //         }
+    //         if (e.target.value == 'csv') {
+    //             setInput('csv')
+    //         }
+    //     } else {
+    //         setInput(false)
+    //     }
+    // }
 
     useEffect(() => {
 
@@ -49,96 +83,139 @@ const Dashboard = () => {
 
     return (
         <div>
-            <form id='processEmailForm' onSubmit={handleSubmit}>
-                <section>
-                    <h2>Settings</h2>
-                    <input type='text' name='originating_crm' placeholder='Originating CRM' value={values.originating_crm} onChange={handleChange} onBlur={handleBlur} />
-                    {
-                        errors.originating_crm && touched.originating_crm &&
-                        <span>{errors.originating_crm}</span>
-                    }
-                    <br />
-                    <input type='text' name='new_crm' placeholder='New CRM' value={values.new_crm} onChange={handleChange} onBlur={handleBlur} />
-                    {
-                        errors.new_crm && touched.new_crm &&
-                        <span>{errors.new_crm}</span>
-                    }
-                    <br />
-                    <input type='text' name='new_campaign' placeholder='New Campaigm' value={values.new_campaign} onChange={handleChange} onBlur={handleBlur} />
-                    {
-                        errors.new_campaign && touched.new_campaign &&
-                        <span>{errors.new_campaign}</span>
-                    }
-                    <br />
-                    <input type='text' name='new_product' placeholder='New Product' value={values.new_product} onChange={handleChange} onBlur={handleBlur} />
-                    {
-                        errors.new_product && touched.new_product &&
-                        <span>{errors.new_product}</span>
-                    }
-                    <br />
-                    <input type='text' name='max_per_day' placeholder='Max Per Day' value={values.max_per_day} onChange={handleChange} onBlur={handleBlur} />
-                    {
-                        errors.max_per_day && touched.max_per_day &&
-                        <span>{errors.max_per_day}</span>
-                    }
-                    <br />
-                    <input type='time' name='start_hours' placeholder='Start Hours' value={values.start_hours} onChange={handleChange} onBlur={handleBlur} />
-                    {
-                        errors.start_hours && touched.start_hours &&
-                        <span>{errors.start_hours}</span>
-                    }
-                    <br />
-                    <input type='time' name='start_minutes' placeholder='Start Minutes' value={values.start_minutes} onChange={handleChange} onBlur={handleBlur} />
-                    {
-                        errors.start_minutes && touched.start_minutes &&
-                        <span>{errors.start_minutes}</span>
-                    }
-                    <br />
-                    <input type='time' name='end_hours' placeholder='End Hours' value={values.end_hours} onChange={handleChange} onBlur={handleBlur} />
-                    {
-                        errors.end_hours && touched.end_hours &&
-                        <span>{errors.end_hours}</span>
-                    }
-                    <br />
-                    <input type='time' name='end_minutes' placeholder='End Minutes' value={values.end_minutes} onChange={handleChange} onBlur={handleBlur} />
-                    {
-                        errors.end_minutes && touched.end_minutes &&
-                        <span>{errors.end_minutes}</span>
-                    }
-                    <br />
+            <div className='container'>
+                {
+                    successMessage &&
+                    <div id="alert-border-3" className="flex items-center p-4 mb-4 text-green-800 border-t-4 border-green-300 bg-green-50" role="alert">
+                    <svg className="flex-shrink-0 w-4 h-4" aria-hidden="true" xmlns="http://www.w3.org/2000/svg" fill="currentColor" viewBox="0 0 20 20">
+                        <path d="M10 .5a9.5 9.5 0 1 0 9.5 9.5A9.51 9.51 0 0 0 10 .5ZM9.5 4a1.5 1.5 0 1 1 0 3 1.5 1.5 0 0 1 0-3ZM12 15H8a1 1 0 0 1 0-2h1v-3H8a1 1 0 0 1 0-2h2a1 1 0 0 1 1 1v4h1a1 1 0 0 1 0 2Z" />
+                    </svg>
+                    <div className="ml-3 text-sm font-medium">
+                        {successMessage}
+                    </div>
+                </div>
+                }
+                <form id='processEmailForm' onSubmit={handleSubmit} className='flex justify-between'>
+                    <section className='first_col bg-white'>
+                        <h2 className="cmn_head text-xl md:text-2xl text-black font-bold mb-2.5">Settings</h2>
+                        <input type='text' name='pluginName' placeholder='Plugin Name' value={values.pluginName} onChange={handleChange} onBlur={handleBlur} />
+                        <br />
+                        {
+                            errors.pluginName && touched.pluginName &&
+                            <span className="block text-red-500">{errors.pluginName}</span>
+                        }
+                        <br />
+                        <input type='number' name='campaignId' placeholder='Campaign ID' value={values.campaignId} onChange={handleChange} onBlur={handleBlur} />
+                        <br />
+                        {
+                            errors.campaignId && touched.campaignId &&
+                            <span className="block text-red-500">{errors.campaignId}</span>
+                        }
+                        <br />
+                        <input type='number' name='affId' placeholder='Affiliate ID' value={values.affId} onChange={handleChange} onBlur={handleBlur} />
+                        <br />
+                        {
+                            errors.affId && touched.affId &&
+                            <span className="block text-red-500">{errors.affId}</span>
+                        }
+                        <br />
+                        <input type='number' name='product1_id' placeholder='Product ID' value={values.product1_id} onChange={handleChange} onBlur={handleBlur} />
+                        <br />
+                        {
+                            errors.product1_id && touched.product1_id &&
+                            <span className="block text-red-500">{errors.product1_id}</span>
+                        }
+                        <br />
 
-                    <select name='matched' onChange={(e) => {
-                        handleChange(e);
-                        selectChangeHandler(e);
-                    }} onBlur={handleBlur}>
-                        <option defaultValue={''} value=''>Select an option</option>
-                        <option value="email">Email</option>
-                        <option value="csv">CSV</option>
-                    </select>
-                    {
-                        errors.matched && touched.matched &&
-                        <span>{errors.matched}</span>
-                    }
-                    <br />
+                        <input type='number' name='product1_qty' placeholder='Product Quantity' value={values.product1_qty} onChange={handleChange} onBlur={handleBlur} />
+                        <br />
+                        {
+                            errors.product1_qty && touched.product1_qty &&
+                            <span className="block text-red-500">{errors.product1_qty}</span>
+                        }
+                        <br />
 
-                    {
-                        showInput && showInput == 'email' &&
-                        <textarea value={values.emails_list} name='emails_list' onChange={handleChange} onBlur={handleBlur}>
-                        </textarea>
-                    }
+                        <input type='test' name='salesUrl' placeholder='Sales URL' value={values.salesUrl} onChange={handleChange} onBlur={handleBlur} />
+                        <br />
+                        {
+                            errors.salesUrl && touched.salesUrl &&
+                            <span className="block text-red-500">{errors.salesUrl}</span>
+                        }
+                        <br />
 
-                    {
-                        showInput && showInput == 'csv' &&
-                        <input type="file" accept='.csv' name="csv" value={values.csv} onChange={handleChange} onBlur={handleBlur} />
-                    }
+                        <input type='number' name='maxPerDay' placeholder='Max Per Day' value={values.maxPerDay} onChange={handleChange} onBlur={handleBlur} />
+                        <br />
+                        {
+                            errors.maxPerDay && touched.maxPerDay &&
+                            <span className="block text-red-500">{errors.maxPerDay}</span>
+                        }
+                        <br />
+                        <ul className='time_box'>
+                            <li><input type='datetime-local' name='startHour' placeholder='Start Hours' value={values.startHour} onChange={handleChange} onBlur={handleBlur} />
+                                {
+                                    errors.startHour && touched.startHour &&
+                                    <span className="block text-red-500">{errors.startHour}</span>
+                                }
+                            </li>
 
-                </section>
+                            <li><input type='datetime-local' name='startMins' placeholder='Start Minutes' value={values.startMins} onChange={handleChange} onBlur={handleBlur} />
+                                {
+                                    errors.startMins && touched.startMins &&
+                                    <span className="block text-red-500">{errors.startMins}</span>
+                                }
+                            </li>
 
-                <section>
-                    <button type='submit'>Process</button>
-                </section>
+                            <li className=''><input type='datetime-local' name='endHour' placeholder='End Hours' value={values.endHour} onChange={handleChange} onBlur={handleBlur} />
+                                {
+                                    errors.endHour && touched.endHour &&
+                                    <span className="block text-red-500">{errors.endHour}</span>
+                                }
+                            </li>
 
-            </form>
+                            <li><input type='datetime-local' name='endMins' placeholder='End Minutes' value={values.endMins} onChange={handleChange} onBlur={handleBlur} />
+                                {
+                                    errors.endMins && touched.endMins &&
+                                    <span className="block text-red-500">{errors.endMins}</span>
+                                }
+                            </li>
+
+                        </ul>
+                        <div className='clearfix'></div>
+                    </section>
+                    <section className='second_col bg-white'>
+                        {/* <select onChange={(e) => {
+                            handleChange(e);
+                            selectChangeHandler(e);
+                        }} onBlur={handleBlur}>
+                            <option defaultValue={''} value=''>Select an option</option>
+                            <option value="email">Email</option>
+                            <option value="csv">CSV</option>
+                        </select>
+                        {
+                            errors.matched && touched.matched &&
+                            <span className="block text-red-500">{errors.matched}</span>
+                        } */}
+                        <h2 className="cmn_head text-xl md:text-2xl text-black font-bold mb-2.5">E-mails for reprocessing</h2>
+                        {/* {
+                            showInput && showInput == 'email' &&
+                            <textarea className='cmn_box' cols="30" rows="10" value={values.emails_list} name='emails_list' onChange={handleChange} onBlur={handleBlur}>
+                            </textarea>
+                        } */}
+
+                        {
+                            // showInput && showInput == 'csv' &&
+                            <input ref={csvRef} type="file" accept='.csv' name="csvFile" onChange={(event) => {
+                                setFieldValue("csvFile", event.currentTarget.files[0]);
+                            }} onBlur={handleBlur} />
+                        }
+                        {
+                            errors.csvFile && touched.csvFile &&
+                            <span className="block text-red-500">{errors.csvFile}</span>
+                        }
+                        <button type='submit' className='submit_btn block ml-auto rounded-3xl'>Process</button>
+                    </section>
+                </form>
+            </div>
         </div>
     )
 }
--- src/schemas/index.jsx
+++ src/schemas/index.jsx
@@ -1,14 +1,39 @@
 import * as Yup from 'yup';
 
+// const validFileExtensions = { file: ['csv'] };
+
+// function isValidFileType(fileName, fileType) {
+//   return fileName && validFileExtensions[fileType].indexOf(fileName.split('.').pop()) > -1;
+// }
+
 export const processEmailSchema = Yup.object({
-    originating_crm:Yup.string().required('Please enter a value'),
-    new_crm:Yup.string().required('Please enter a value'),
-    new_campaign:Yup.string().required('Please enter a value'),
-    new_product:Yup.string().required('Please enter a value'),
-    max_per_day:Yup.string().required('Please enter a value'),
-    start_hours:Yup.string().required('Please enter a value'),
-    start_minutes:Yup.string().required('Please enter a value'),
-    end_hours:Yup.string().required('Please enter a value'),
-    end_minutes:Yup.string().required('Please enter a value'),
-    matched:Yup.string().required('Please enter a value'),
+    pluginName:Yup.string().required('Please enter plugin name'),
+    campaignId:Yup.number().required('Please enter campaign ID').test(
+        'Is positive?', 
+        'The campaign ID must be greater than 0!', 
+        (value) => value > 0
+    ),
+    affId:Yup.string().required('Please enter Affiliate ID'),
+    product1_id:Yup.number().required('Please enter product ID').test(
+        'Is positive?', 
+        'The product ID must be greater than 0!', 
+        (value) => value > 0
+    ),
+    product1_qty:Yup.number().required('Please enter product quantity').test(
+        'Is positive?', 
+        'The product quantity must be greater than 0!', 
+        (value) => value > 0
+    ),
+    salesUrl:Yup.string().required('Please enter sales URL').url('Invalid sales URL'),
+    maxPerDay:Yup.number('Must be a number').required('Please enter max per day limit').test(
+        'Is positive?', 
+        'The max per day limit must be greater than 0!', 
+        (value) => value > 0
+    ),
+    startHour:Yup.string().required('Start hour is required'),
+    startMins:Yup.string().required('Start Minute is required'),
+    endHour:Yup.string().required('End hour is required'),
+    endMins:Yup.string().required('End Minute is required'),
+    // matched:Yup.string().required('Please enter a value'),
+    csvFile:Yup.mixed().required("Please choose a CSV file")
 });
